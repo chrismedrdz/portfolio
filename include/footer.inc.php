dbdafd03fcbaaa48f2b0ec337b696ba38f822f2a
Christopher Medina <chris.medrdz@gmail.com>
2016-02-22 19:33:11 -0600

    Initil commit
    Portafolio

---
+++ include/footer.inc.php
@@ -0,0 +1,51 @@
+<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights" style="background-color: rgba(0,0,0,0.9); color: white;">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						&copy; <?php echo date('Y');?> Todos los Derechos Reservados por Christopher Medina.<br>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+							<a target="_blank" href="https://www.linkedin.com/in/chrismedrdz" class="social-icon si-small si-borderless si-linkedin">
+								<i class="icon-linkedin" style="color:white!important;"></i>
+								<i class="icon-linkedin"></i>
+							</a>
+
+							<a target="_blank" href="https://twitter.com/ChrisDevlpr" class="social-icon si-small si-borderless si-twitter">
+								<i class="icon-twitter" style="color:white!important;"></i>
+								<i class="icon-twitter"></i>
+							</a>							
+
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> chris.medrdz@gmail.com <span class="middot">&middot;</span> <i class="icon-call"></i> +52 (01) 81 200 27551 <span class="middot">&middot;</span> <i class="icon-skype2"></i> chris.medrdz
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
